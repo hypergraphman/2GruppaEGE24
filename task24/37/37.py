c389f2c6eacf9f75f3fd90135ae35269d62bb136
Руслан Аитов <r_aitov@mail.ru>
2024-02-21 19:17:39 +0300

    task24

--- task24/37/37.py
+++ task24/37/37.py
@@ -2,6 +2,6 @@ s = open('24.txt').readline().strip()
 a = [i for i in range(1, len(s) - 1) if s[i - 1] + s[i] + s[i + 1] == '131']
 mx = 0
 for i in range(len(a) - 132):
-    if a[i + 132] - a[i] - 1 > mx:
+    if a[i + 132] - a[i] + 1 > mx:
         mx = a[i + 132] - a[i] + 1
 print(mx)
