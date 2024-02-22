ed1c7143da403f3a552306ff806e97b2649a500d
Руслан Аитов <r_aitov@mail.ru>
2024-02-22 17:57:21 +0300

    task16

---
+++ task16/6.py
@@ -0,0 +1,14 @@
+def f(q, p):
+    if p == 0:
+        return q
+    if p > 0:
+        return f(p, q % p)
+
+
+k = 0
+for q in range(50, 250 + 1):
+    for p in range(50, 250 + 1):
+        if f(q, p) == 25:
+            k += 1
+            break
+print(k)
